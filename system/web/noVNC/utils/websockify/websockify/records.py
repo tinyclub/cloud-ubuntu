0cb30d345857fde6d4c9e9b907dcce477f92fd71
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-15 22:16:48 +0800

    web: novnc: play: reimplement the records class
    TODO:
    
    1. compress the data with zlib+base64: VNC_frame_data_size/VNC_frame_data_compressed
    2. split the data to several pieces: VNC_frame_data_part feature.
    3. compress the slices with zlib+base64: VNC_frame_data_size/VNC_frame_data_compressed (the same?)
       VNC_frame_data_size/VNC_frame_data_compressed --> VNC_frame_data_part
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/web/noVNC/utils/websockify/websockify/records.py
+++ system/web/noVNC/utils/websockify/websockify/records.py
@@ -3,10 +3,12 @@
 import os, re, time
 
 class Records:
-    def __init__(self, record_dir = 'recordings/', record_list = 'records.js', record_html = 'records.html'):
+    def __init__(self, record_dir = 'recordings/', record_list = 'records.js', record_html = 'records.html', record_slice_size = 128):
 	self.record_dir = record_dir
 	self.record_list = record_list
 	self.record_html = record_html
+	# in KB
+	self.record_slice_size = 128 * 1024
 
     def compare(self, x, y):
 	stat_x = os.stat(self.record_dir + "/" + x)
@@ -24,7 +26,7 @@ class Records:
 	content = "var VNC_record_player = '/play.html';\n"
 	content += "var VNC_record_dir = '/%s';\n\n" % os.path.basename(self.record_dir.strip('/'))
 	content += "var VNC_record_data = [ \n"
-	content += "  ['Name', 'Title', 'Size', 'Create', 'Author', 'Tags', 'Desc'],\n"
+	content += "  ['Name', 'Title', 'Size', 'Time', 'Create', 'Author', 'Tags', 'Desc'],\n"
 
 	rec_list = os.listdir(os.path.abspath(self.record_dir))
 	# sort by time
@@ -32,6 +34,7 @@ class Records:
 
 	num = 0;
 	for rec in rec_list:
+	    print "Log: " + rec
 	    if (rec == self.record_list or rec == self.record_html):
 		continue;
 
@@ -39,24 +42,48 @@ class Records:
 	    f = os.path.abspath(self.record_dir + rec)
 	    t = open(f)
 
-	    frame_data_valid = 0
-	    info = {"create": '', "title": '', 'author': '', 'tags': '', 'desc': ''}
-	    for i in range(1, 20):
-		l = t.readline()
-		if not l:
-		    break
+	    # Init the variables: VNC_frame_xxx
+	    # VNC_frame_data_size/VNC_frame_data_compressed
+	    #
+	    # data size have different meanings:
+	    #   0. the whole file size, include the header variables
+	    #   1. original data size in string
+	    #   2. array data size in binary
+	    #   3. joined array data size for compress (required by client for decompression)
+	    #   4. data size after compression
 
-		for (k, v) in info.items():
-		    if not v:
-			m = re.match(r"var VNC_frame_%s = '(.*)';" % k, l)
-			if m and len(m.groups()):
-			    info[k] = m.group(1)
+	    info = {"create": '', "title": '', 'author': '', 'tags': '', 'desc': '', 'encoding': '', 'length': 0, 'time': 0, 'parts': 0, 'data_size': 0}
+	    for (k, v) in info.items():
+		exec("VNC_frame_%s = ''" % k)
 
-	 	m = re.match(r"var VNC_frame_encoding = '(.*)';", l)
+	    # Convert to python code
+	    py_data = t.read().replace('var VNC_', 'VNC_')
+	    exec(py_data)
+
+	    if globals().has_key('VNC_frame_data') or locals().has_key('VNC_frame_data'):
+		# The number of frames
+	        VNC_frame_length = len(VNC_frame_data)
+		# print "LOG: VNC_frame_length = %s" % VNC_frame_length
+
+		# Match strings like '{62911{\x00@' or '}304}RFB 003.008' to get out of the timestamp
+		m = re.match(r'[{}]([0-9]{1,})[{}]',VNC_frame_data[VNC_frame_length-2])
 		if m and len(m.groups()):
-		    frame_data_valid = 1
+		    VNC_frame_time = time.strftime("%H:%M:%S", time.gmtime(float(m.group(1))/1000))
+		    # print "LOG: > VNC_frame_time = %s" % VNC_frame_time
+	    else:
+		if str(VNC_frame_time).isdigit():
+		    VNC_frame_time = time.strftime("%H:%M:%S", time.gmtime(float(VNC_frame_time)/1000))
+		    # print "LOG: < VNC_frame_time = %s" % VNC_frame_time
 
-	    if not frame_data_valid:
+		# The size in string, not in binary
+		# VNC_frame_data_size = len(repr(VNC_frame_data))
+
+	    for (k, v) in info.items():
+		val = eval("VNC_frame_%s" % k)
+		if val:
+		    info[k] = val
+
+	    if not info['encoding']:
 		print "Invalid noVNC session data: %s" % rec
 		t.close()
 		continue
@@ -70,7 +97,7 @@ class Records:
 	    if not info['title']:
 		info["title"] = rec
 	    if not info['author']:
-		info['author'] = "anonymity"
+		info['author'] = "Unknown"
 	    if not info['tags']:
 		info['tags'] = ""
 	    if not info['desc']:
@@ -89,9 +116,13 @@ class Records:
 		unit = "M"
 
 	    content += \
-		   "  ['%s', '%s', '%s%s', '%s', '%s', '%s', '%s'],\n" \
-		   % (rec, info['title'], str(rec_size), unit,
+		   "  ['%s', '%s', '%s%s', '%s', '%s', '%s', '%s', '%s'],\n" \
+		   % (rec, info['title'], str(rec_size), unit, info['time'],
 		     info['create'], info['author'], info['tags'], info['desc'])
+
+	    for k in ['data', 'data_compressed', 'data_part']:
+		if globals().has_key('VNC_frame_%s' % k) or locals().has_key('VNC_frame_%s' % k):
+		    exec("del VNC_frame_%s" % k)
 
 	content += "];";
 
