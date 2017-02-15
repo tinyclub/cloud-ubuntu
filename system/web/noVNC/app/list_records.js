d610e9dfbb78ddff867f613b7a37b8dfe10501c5
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-15 22:16:48 +0800

    web: novnc: fix up for load a new data after playing the .slice
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/web/noVNC/app/list_records.js
+++ system/web/noVNC/app/list_records.js
@@ -1,4 +1,4 @@
-function list_records() {
+function list_records(theme) {
     var data = new Array();
     var record = VNC_record_data;
     var player = VNC_record_player;
@@ -10,6 +10,7 @@ function list_records() {
     data.push("<tr class='head'>\n");
     data.push("<th> No. </th>\n");
     data.push("<th> Play </th>\n");
+    data.push("<th> Speedup </th>\n");
     data.push("<th> Down </th>\n");
 
     var i, head = record[0];
@@ -27,9 +28,16 @@ function list_records() {
 
         row = record[i];
 
-        play_url = player + "?data=" + row[0];
         down_url = record_dir + "/" + row[0];
-        data.push("<td><a href='" + play_url + "'>&gt;</a></td>\n");
+	if (theme) {
+            data.push("<td><input type='radio' name='session' onclick='load(" + i + ");'></td>\n");
+            data.push("<td><input type='radio' name='session' onclick='load(" + i + ", \".slice\");'></td>\n");
+	} else {
+            play_url = player + "?data=" + row[0];
+            data.push("<td><a href='" + play_url + "'>&gt;</a></td>\n");
+            play_url = player + "?data=" + row[0] + ".slice";
+            data.push("<td><a href='" + play_url + "'>&gt;</a></td>\n");
+	}
         data.push("<td><a href='" + down_url + "'>v</a></td>\n");
 
         for (j = 1; j < row.length; j++)
@@ -45,46 +53,5 @@ function list_records() {
 
 
 function draw_records() {
-    var data = new Array();
-    var record = VNC_record_data;
-    var player = VNC_record_player;
-    var record_dir = VNC_record_dir;
-    var target = document.getElementById('VNC_records');
-
-    data.push("<table>\n")
-
-    data.push("<tr class='head'>\n");
-    data.push("<th> No. </th>\n");
-    data.push("<th> Play </th>\n");
-    data.push("<th> Down </th>\n");
-
-    var i, head = record[0];
-    for (i = 1; i < head.length; i++)
-        data.push("<th>" + head[i] + "</th>\n");
-    data.push("</tr>\n");
-
-    var bg, row, j, play_url, down_url;
-    for (i = 1; i < record.length; i++) {
-        bg = "even";
-        if (i % 2 === 0)
-            bg = "odd";
-        data.push("<tr class='" + bg + "'>\n");
-        data.push("<td> " + i + " </td>\n");
-
-        row = record[i];
-
-        play_url = player + "?data=" + row[0];
-        down_url = record_dir + "/" + row[0];
-        data.push("<td><input type='radio' name='session' onclick='load(" + i + ");'></td>\n");
-        data.push("<td><a href='" + down_url + "'>v</a></td>\n");
-
-        for (j = 1; j < row.length; j++)
-            data.push("<td>" + row[j] + "</td>\n");
-
-        data.push("</tr>\n");
-    }
-
-    data.push("</table>")
-
-    target.innerHTML = data.join('');
+    list_records(1);
 }
