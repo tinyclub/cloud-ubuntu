ebb340a049f2f3f49983e26532e418cb4e02f745
Wu Zhangjin <wuzhangjin@gmail.com>
2017-03-08 22:16:48 +0800

    web: gateone: fix up variables setting
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- system/web/etc/startup.aux/gateone.sh
+++ system/web/etc/startup.aux/gateone.sh
@@ -6,7 +6,7 @@
 AUTH_CONF=/etc/gateone/conf.d/20authentication.conf
 TERM_CONF=/etc/gateone/conf.d/50terminal.conf
 
-[ -z "GATEONE_PUBLIC" ] && GATEONE_PUBLIC=0
+[ -z "$GATEONE_PUBLIC" ] && GATEONE_PUBLIC=0
 
 echo "GATEONE_PUBLIC: " $GATEONE_PUBLIC
 echo "GATEONE_AUTH: " $GATEONE_AUTH
