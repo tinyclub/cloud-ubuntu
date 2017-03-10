5542d8a3cd9136f2dfccdc1b20b4f7b5554f1cf3
Heiher <r@hev.cc>
2017-03-10 10:35:45 +0800

    Update tproxy
    Signed-off-by: Heiher <r@hev.cc>

--- system/proxy_client_transparent/etc/startup.aux/start-proxy-client.sh
+++ system/proxy_client_transparent/etc/startup.aux/start-proxy-client.sh
@@ -16,9 +16,15 @@ TOP_DIR=$(cd $(dirname $0) && pwd)/../..
 PROXY_SERVER_IP=${PROXY_SERVER%:*}
 PROXY_SERVER_PORT=${PROXY_SERVER#*:}
 
-HPROXY_PASS=$PROXY_PWD ${TOP_DIR}/usr/local/bin/hev-socks5-client 0.0.0.0 $PROXY_PORT $PROXY_SERVER_IP $PROXY_SERVER_PORT &
+echo -e "[Main]\nWorkers=4\nPort=${PROXY_PORT}\nListenAddress=0.0.0.0\n" >> \
+	${TOP_DIR}/etc/hev-socks5-client.conf
+echo -e "[Srv1]\nPort=${PROXY_SERVER_PORT}\nAddress=${PROXY_SERVER_IP}\nPassword=${PROXY_PWD}\n" >> \
+	${TOP_DIR}/etc/hev-socks5-client.conf
+echo -e "[Socks5]\nPort=${PROXY_PORT}\nAddress=127.0.0.1\n" >> \
+	${TOP_DIR}/etc/hev-socks5-tproxy.conf
 
-${TOP_DIR}/usr/local/bin/hev-socks5-tproxy 0.0.0.0 10800 0.0.0.0 53 127.0.0.1 $PROXY_PORT &
+${TOP_DIR}/usr/local/bin/hev-socks5-client ${TOP_DIR}/etc/hev-socks5-client.conf &
+${TOP_DIR}/usr/local/bin/hev-socks5-tproxy ${TOP_DIR}/etc/hev-socks5-tproxy.conf &
 
 sudo iptables-restore ${TOP_DIR}/etc/iptables/tproxy.rules
 sudo iptables -t nat -I OUTPUT -d $PROXY_SERVER_IP/32 -p tcp --dport $PROXY_SERVER_PORT -j RETURN
